4f0bdcb1cdaf50f01237228dea0a6ae56afec20a
Andrew <32229265+aselee@users.noreply.github.com>
2018-05-09 16:49:25 -0500

    syntax correction

--- server/Controllers/vehicleController.js
+++ server/Controllers/vehicleController.js
@@ -12,7 +12,7 @@ module.exports.list =  function list(request, response) {
     return response.json([vehicles]);
    }
 module.exports.show =  function show(request, response) {
-    return response.json({vehicles.find(user => user._id == request.params.id});
+    return response.json(vehicles.find(user => user._id == request.params.id);
    }
 module.exports.create =  function create(request, response) {
     const conVehicles = request.body;
