63cfca54409492fefa83ed3186f21eac2ab91982
Andrew <32229265+aselee@users.noreply.github.com>
2018-05-09 16:48:13 -0500

    syntax correction

--- server/Controllers/commentController.js
+++ server/Controllers/commentController.js
@@ -12,7 +12,7 @@ module.exports.list =  function list(request, response) {
     return response.json([comments]);
    }
 module.exports.show =  function show(request, response) {
-    return response.json({comments.find(user => user._id == request.params.id});
+    return response.json(comments.find(user => user._id == request.params.id);
    }
 module.exports.create =  function create(request, response) {
     const conComments = request.body;
