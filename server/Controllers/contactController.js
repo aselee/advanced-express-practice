e9013bb8704e02f5ff0648b56dbc8bcd615b0d4c
Andrew <32229265+aselee@users.noreply.github.com>
2018-05-14 18:08:30 -0500

    part 4

--- server/Controllers/contactController.js
+++ server/Controllers/contactController.js
@@ -5,20 +5,41 @@
 // * create functions for list, show and create
 // * Import and use the controller functions in the appropiate Router
 
-let contacts = require("../contacts");
-
+let comments = require("../Models/contactModel");
 
 module.exports.list =  function list(request, response) {
-    return response.json([contacts]);
-   }
-module.exports.show =  function show(request, response) {
-    return response.json(contacts.find(user => user._id == request.params.id));
-   }
-module.exports.create =  function create(request, response) {
-    const conContact = request.body;
-    contacts.push(conContact);
-    return response.json({conContact});
+    contactModel.find({}).exec()
+    .then(contacts => {
+        response.json(contacts);
+    });
    };
+module.exports.show =  function show(request, response) {
+    contactModel.findById(request.params.id).exec()
+    .then(contacts => {
+        res.json(contacts);
+    });
+   };
+module.exports.create =  function create(request, response) {
+    const conContacts = new contactModel(request.body);
+    conContacts.save()
+    .then(savedContacts => {
+        response.json(savedContacts);
+    });
+   };
+
+
+
+// module.exports.list =  function list(request, response) {
+//     return response.json([contacts]);
+//    }
+// module.exports.show =  function show(request, response) {
+//     return response.json(contacts.find(user => user._id == request.params.id));
+//    }
+// module.exports.create =  function create(request, response) {
+//     const conContact = request.body;
+//     contacts.push(conContact);
+//     return response.json({conContact});
+//    };
 // module.exports.update =  function update(request, response) {
 //     return response.json({theId: request.params.id});
 //    }
