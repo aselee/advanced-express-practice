2dd1bdcbb2b69a18fad0c4f328e7de98050d7cca
Andrew <32229265+aselee@users.noreply.github.com>
2018-05-14 18:10:21 -0500

    part 4

--- server/Controllers/vehicleController.js
+++ server/Controllers/vehicleController.js
@@ -5,19 +5,39 @@
 // * create functions for list, show and create
 // * Import and use the controller functions in the appropiate Router
 
-let vehicles = require("../vehicles");
-
+let comments = require("../Models/vehicleModel");
 
 module.exports.list =  function list(request, response) {
-    return response.json([vehicles]);
-   }
+    vehicleModel.find({}).exec()
+    .then(vehicles => {
+        response.json(vehicles);
+    });
+   };
 module.exports.show =  function show(request, response) {
-    return response.json(vehicles.find(user => user._id == request.params.id));
-   }
+    vehicleModel.findById(request.params.id).exec()
+    .then(vehicles => {
+        res.json(vehicles);
+    });
+   };
 module.exports.create =  function create(request, response) {
-    const conVehicles = request.body;
-    comments.push(conVehicles);
-    return response.json({conVehciles});
+    const conVehicles = new vehicleModel(request.body);
+    conVehicles.save()
+    .then(savedVehicles => {
+        response.json(savedVehicles);
+    });
+   };
+
+
+// module.exports.list =  function list(request, response) {
+//     return response.json([vehicles]);
+//    }
+// module.exports.show =  function show(request, response) {
+//     return response.json(vehicles.find(user => user._id == request.params.id));
+//    }
+// module.exports.create =  function create(request, response) {
+//     const conVehicles = request.body;
+//     comments.push(conVehicles);
+//     return response.json({conVehciles});
    };
 // module.exports.update =  function update(request, response) {
 //     return response.json({theId: request.params.id});
