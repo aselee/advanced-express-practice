eee8afa4bd2c2d7859525b8fc8b371ab7e2fc6a4
Andrew <32229265+aselee@users.noreply.github.com>
2018-05-14 18:09:01 -0500

    part 4

--- server/Controllers/productController.js
+++ server/Controllers/productController.js
@@ -5,20 +5,40 @@
 // * create functions for list, show and create
 // * Import and use the controller functions in the appropiate Router
 
-let products = require("../products");
-
+let comments = require("../Models/productModel");
 
 module.exports.list =  function list(request, response) {
-    return response.json([products]);
-   }
-module.exports.show =  function show(request, response) {
-    return response.json(products.find(user => user._id == request.params.id));
-   }
-module.exports.create =  function create(request, response) {
-    const conProducts = request.body;
-    comments.push(conProducts);
-    return response.json({conProducts});
+    productModel.find({}).exec()
+    .then(products => {
+        response.json(products);
+    });
    };
+module.exports.show =  function show(request, response) {
+    productModel.findById(request.params.id).exec()
+    .then(products => {
+        res.json(products);
+    });
+   };
+module.exports.create =  function create(request, response) {
+    const conProducts = new productModel(request.body);
+    conProducts.save()
+    .then(savedProducts => {
+        response.json(savedProducts);
+    });
+   };
+
+
+// module.exports.list =  function list(request, response) {
+//     return response.json([products]);
+//    }
+// module.exports.show =  function show(request, response) {
+//     return response.json(products.find(user => user._id == request.params.id));
+//    }
+// module.exports.create =  function create(request, response) {
+//     const conProducts = request.body;
+//     comments.push(conProducts);
+//     return response.json({conProducts});
+//    };
 // module.exports.update =  function update(request, response) {
 //     return response.json({theId: request.params.id});
 //    }
