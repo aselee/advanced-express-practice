e46d4741c647916a8e9c4c99b9899e1a9cc94459
Andrew <32229265+aselee@users.noreply.github.com>
2018-05-14 18:07:51 -0500

    part 4

--- server/Controllers/commentController.js
+++ server/Controllers/commentController.js
@@ -5,20 +5,45 @@
 // * create functions for list, show and create
 // * Import and use the controller functions in the appropiate Router
 
-let comments = require("../comments");
-
+let comments = require("../Models/commentModel");
 
 module.exports.list =  function list(request, response) {
-    return response.json([comments]);
-   }
-module.exports.show =  function show(request, response) {
-    return response.json(comments.find(user => user._id == request.params.id));
-   }
-module.exports.create =  function create(request, response) {
-    const conComments = request.body;
-    comments.push(conComments);
-    return response.json({conComments});
+    commentModel.find({}).exec()
+    .then(comments => {
+        response.json(comments);
+    });
    };
+module.exports.show =  function show(request, response) {
+    commentModel.findById(request.params.id).exec()
+    .then(comments => {
+        res.json(comments);
+    });
+   };
+module.exports.create =  function create(request, response) {
+    const conComments = new commentModel(request.body);
+    conComments.save()
+    .then(savedComments => {
+        response.json(savedComments);
+    });
+   };
+
+
+
+
+
+
+
+// module.exports.list =  function list(request, response) {
+//     return response.json(comments);
+//    }
+// module.exports.show =  function show(request, response) {
+//     return response.json(comments.find(user => user._id == request.params.id));
+//    }
+// module.exports.create =  function create(request, response) {
+//     const conComments = request.body;
+//     comments.push(conComments);
+//     return response.json({conComments});
+//    };
 // module.exports.update =  function update(request, response) {
 //     return response.json({theId: request.params.id});
 //    }
